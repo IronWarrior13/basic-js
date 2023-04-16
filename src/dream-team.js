7ff3d67541bf69b96372abfc3e7bad837610f2a5
IronWarrior13 <40057284+IronWarrior13@users.noreply.github.com>
2023-04-16 16:04:50 +0300

    feat: dream-team test complited

--- src/dream-team.js
+++ src/dream-team.js
@@ -15,7 +15,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function createDreamTeam(/* members */) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let teamName = [];
+    members.forEach(element => {
+        if(typeof element === `string`)
+            teamName.push(element[0]);
+    });
+    return teamName.sort().join(``);
 }
 
 module.exports = {
